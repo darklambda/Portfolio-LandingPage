2661148e5011392df83a5d5b6e0822c33547169c
darklambda <usergonzalo@hotmail.com>
2023-11-25 18:10:31 +0100

    some trajectory made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatMenuModule, MatButtonModule,
-            RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, 
+            MatMenuModule, MatButtonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'HomePage';
+  title = 'landingPage';
 }
--- src/app/trajectory/trajectory.component.ts
+++ src/app/trajectory/trajectory.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-trajectory',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './trajectory.component.html',
   styleUrl: './trajectory.component.css'
 })
 export class TrajectoryComponent {
-
+  events = [];
+  
 }
