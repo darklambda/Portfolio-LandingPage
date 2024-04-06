e0ee4986c58929212e15c72547a457148cf10583
darklambda <usergonzalo@hotmail.com>
2024-04-06 19:56:43 +0200

    Update - Social networks added and other minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,29 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, 
-            MatMenuModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule,
+            MatMenuModule, MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  toGithub(): void {
+    window.location.href = "http://github.com/darklambda"
+  }
+  
+  toLinkedin(): void {
+    window.location.href = "http://linkedin.com/in/gonzalo-oberreuter-alvarez"
+  }
+
+  toInstagram(): void {
+    window.location.href = "https://www.instagram.com/el.obe"
+  }
 
   title = 'Home';
 }
