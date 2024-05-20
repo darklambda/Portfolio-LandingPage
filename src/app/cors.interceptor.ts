58ab2b4e57b06629bbf9360ce3420a52ce104dd1
darklambda <usergonzalo@hotmail.com>
2024-05-20 15:45:45 +0200

    Fix in cors v4

--- src/app/cors.interceptor.ts
+++ src/app/cors.interceptor.ts
@@ -2,10 +2,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from './../environments/environment';
 
 export const corsInterceptor: HttpInterceptorFn = (req, next) => {
-  let request = req.clone({
+  req = req.clone({
     setHeaders: {
       'Access-Control-Allow-Origin': '*'
     }
   })
-  return next(request);
+  return next(req);
 };
