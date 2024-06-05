21563dc936b4a54329e1b75fb6c9b81f50c0bff8
darklambda <usergonzalo@hotmail.com>
2024-06-05 23:14:39 +0200

    New text in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { NgIf } from '@angular/common';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, NgIf],
+  imports: [CommonModule, MatGridListModule, MatCardModule, MatTooltipModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
+
 export class HomeComponent{
 
+  email: string = 'usergonzalo@gmail.com'
+
+  @ViewChild("myTooltip") myTooltip!: MatTooltip;
+
+  public displayTooltip(){
+    this.myTooltip.disabled = false;
+    this.myTooltip.show()
+    setTimeout(() => {
+      this.myTooltip.disabled = true;
+    }, 1000);
+  }
 
 }
