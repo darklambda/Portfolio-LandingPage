97b276f51425bb42e8fdcdf41fde4a284411a2c5
darklambda <usergonzalo@hotmail.com>
2024-07-05 02:59:24 +0200

    refactor: fix code style of new component

--- src/app/open-cv/open-cv.component.ts
+++ src/app/open-cv/open-cv.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NgIf } from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-open-cv',
@@ -8,24 +9,28 @@ import { NgIf } from '@angular/common';
   templateUrl: './open-cv.component.html',
   styleUrl: './open-cv.component.css'
 })
-export class OpenCVComponent implements OnInit{
+export class OpenCVComponent implements OnInit {
   
   private peer!: RTCPeerConnection;
+
   public webcamAvailable = true;
   public webcamOnUse = false;
-  public mediaStream: MediaStream | undefined;
   public permissionsDenied = false;
+
+  public mediaStream: MediaStream | undefined;
+  public streamAvailable: boolean = false;
+
   
   ngOnInit(): void {
     OpenCVComponent.getAvailableVideoInputs()
-    .then((mediaDevices: MediaDeviceInfo[]) => {
-      this.webcamAvailable = mediaDevices && mediaDevices.length > 0;
-    },
-      (error) => this.webcamAvailable = false);
+    .then(
+      (mediaDevices: MediaDeviceInfo[]) => {
+        this.webcamAvailable = mediaDevices.length > 0;
+      },
+      () => this.webcamAvailable = false);
   }
 
   public onStart() {
-    console.log("Starting stream")
     if (!this.mediaStream)
       navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(stream  => {
@@ -37,120 +42,114 @@ export class OpenCVComponent implements OnInit{
           this.onStop();
           this.permissionsDenied = true;
         });
-    else console.log("Stream already on");
   }
 
   private startWebRTC() {
     this.createPeerConnection();
-    console.log("Peer created");
-    // Double check that video is available
     this.peer.addTrack(this.mediaStream!.getVideoTracks()[0]);
-    console.log("Track added to peer");
     try {
-      this.negotiate();
+      this.negotiateConnection();
     } catch {
       this.onStop();
     }
-
   }
 
   private createPeerConnection() {
-    const config: RTCConfiguration = {}
+    const pc = new RTCPeerConnection();
 
-    const pc = new RTCPeerConnection(config);
+    pc.addEventListener('icegatheringstatechange', 
+      () => { console.log("Ice Gathering State Change", pc.iceGatheringState) }, 
+      false
+    );
 
-    let tmp: MediaStream;
+    pc.addEventListener('iceconnectionstatechange', 
+      () => { console.log("Ice Connection State Change", pc.iceConnectionState) },
+      false
+    );
 
-    // register some listeners to help debugging
-    pc.addEventListener('icegatheringstatechange', function() {
-        console.log("ice gathering state change", pc.iceGatheringState);
-    }, false);
+    pc.addEventListener('signalingstatechange', 
+      () => { console.log("Signaling State Change", pc.signalingState) },
+      false
+    );
 
-    pc.addEventListener('iceconnectionstatechange', function() {
-      console.log("ice connection state change", pc.iceConnectionState);
-    }, false);
-
-    pc.addEventListener('signalingstatechange', function() {
-      console.log("signaling state change", pc.signalingState);
-    }, false);
-
-    pc.addEventListener('track', function(event) {
-      let remoteVideo = document.getElementsByTagName('video')[0]; //Change your remote 'video element' id here
-      if (remoteVideo) remoteVideo.srcObject = event.streams[0];
+    pc.addEventListener('track', 
+      (event) => {
+        let remoteVideo = document.getElementsByTagName('video')[0];
+        if (remoteVideo) {
+          this.streamAvailable = true;
+          remoteVideo.srcObject = event.streams[0];
+        } 
     });
 
     this.peer = pc;
-}
+  }
 
-  private negotiate() {
-  const pc = this.peer;
-  pc.createOffer().then((offer) => {
-      pc.setLocalDescription(offer);
-  }).then(() => {
-      // wait for ICE gathering to complete
-      return new Promise<void>((resolve) => {
-          if (pc.iceGatheringState === 'complete') {
-              resolve();
-          } else {
-              function checkState() {
-                  if (pc.iceGatheringState === 'complete') {
-                      pc.removeEventListener('icegatheringstatechange', checkState);
-                      resolve();
-                  }
-              }
-              pc.addEventListener('icegatheringstatechange', checkState);
-          }
-      });
-  }).then(() => {
-      var offer = this.peer.localDescription;
-      console.log("offer",offer)
-      if (offer !== null)
-      return fetch('http://localhost:8080/offer', {
-          body: JSON.stringify({
-              sdp: offer.sdp,
-              type: offer.type,
-              video_transform: 'edges'
-          }),
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          method: 'POST'
-      });
-      throw new Error("Empty Body");
-  }).then(function(response) {
-      return response.json();
-  }).then(function(answer) {
-      console.log("Answer", answer)
-      pc.setRemoteDescription(answer);
-  }).catch(function(e) {
-      alert(e);
-      throw e;
-  });
+  private negotiateConnection() {
 
-}
-  public onStop() {
-    if (this.mediaStream) this.mediaStream.getTracks()[0].stop();
-    else console.log("No stream");
     const pc = this.peer;
+
+    pc.createOffer().then(
+      (offer) => pc.setLocalDescription(offer)
+    ).then(() => new Promise<void>(
+                  (resolve) => {
+                    if (pc.iceGatheringState === 'complete') resolve();
+                    else {
+                        function checkState() {
+                            if (pc.iceGatheringState === 'complete') {
+                                pc.removeEventListener('icegatheringstatechange', checkState);
+                                resolve();
+                            }
+                        }
+                        pc.addEventListener('icegatheringstatechange', checkState);
+                    }
+                })
+    ).then(() => {
+        let offer = this.peer.localDescription;
+        if (offer !== null)
+        return fetch(environment.WEB_RTC, {
+            body: JSON.stringify({
+                sdp: offer.sdp,
+                type: offer.type,
+                video_transform: 'edges'
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+        throw new Error("Empty Body");
+    }).then((response) => response.json()
+    ).then((answer) => {
+        console.log("Answer Description", answer)
+        pc.setRemoteDescription(answer);
+    }).catch((error) => {
+        alert(error);
+        throw error;
+    });
+  }
+
+  public onStop() {
+    let remoteVideo = document.getElementsByTagName('video')[0];
+    if (remoteVideo && remoteVideo.srcObject) remoteVideo.srcObject = null;
+
+    if (this.mediaStream) this.mediaStream.getTracks()[0].stop();
+
+    const pc = this.peer;
+
     if (pc) {
-        pc.getTransceivers().forEach(function(transceiver) {
-            if (transceiver && transceiver.stop) {
-                transceiver.stop();
-            }
+        pc.getTransceivers().forEach((transceiver) => {
+          if (transceiver && transceiver.stop) transceiver.stop();  
+        });
+        pc.getSenders().forEach((sender) => {
+          if (sender && sender.track) sender.track.stop();
         });
     }
 
-    // close local audio / video
-    if (pc)
-      pc.getSenders().forEach(function(sender) {
-        if (sender && sender.track) sender.track.stop();
-      });
-
-    // close peer connection
-    setTimeout(function() {
-      if (pc)
-        pc.close();
-    }, 500);
+    setTimeout(
+      () => {
+        if (pc) pc.close();
+      }, 
+      500);
   }
 
   public static getAvailableVideoInputs(): Promise<MediaDeviceInfo[]> {
@@ -168,5 +167,4 @@ export class OpenCVComponent implements OnInit{
         });
     });
   }
-
 }
