0548836ce67afa3d128af2b8d747bb38e1910b01
darklambda <usergonzalo@hotmail.com>
2024-07-17 19:14:41 +0200

    feat: add new site titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,17 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TrajectoryComponent } from './trajectory/trajectory.component';
-import { AboutComponent } from './about/about.component';
 import { PlotsComponent } from './plots/plots.component';
 import { RPCameraComponent } from './rp-camera/rp-camera.component';
 import { OpenCVComponent } from './open-cv/open-cv.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent},
-    {path: "projects", component: ProjectsComponent},
-    {path: "projects/openCV", component: OpenCVComponent},
-    {path: "projects/plots", component: PlotsComponent},
-    {path: "projects/rp-camera", component: RPCameraComponent},
-    {path: "trajectory", component: TrajectoryComponent},
-    {path: "about", component: AboutComponent},
-    {path: "**", component: NotFoundComponent}
+    {path: "", title: "Home", component: HomeComponent},
+    {path: "projects", title: "Projects", component: ProjectsComponent},
+    {path: "projects/openCV", title: "OpenCV", component: OpenCVComponent},
+    {path: "projects/plots", title: "Plots", component: PlotsComponent},
+    {path: "projects/rp-camera", title: "RP Camera", component: RPCameraComponent},
+    {path: "trajectory", title: "Trajectory", component: TrajectoryComponent},
+    {path: "**", title: "404", component: NotFoundComponent}
 
 ];
