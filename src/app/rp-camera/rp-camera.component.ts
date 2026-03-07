7da231db3bfbccb1035b117abd4a4805a04930ae
Gonzalo Oberreuter <gonzalo.oberreuter@gmail.com>
2026-03-07 21:54:57 +0100

    fix: remove stl viewer until it has support for angular 21

--- src/app/rp-camera/rp-camera.component.ts
+++ src/app/rp-camera/rp-camera.component.ts
@@ -1,25 +1,16 @@
 import { Component } from '@angular/core';
-import { StlModelViewerComponent } from 'angular-stl-model-viewer';
 import { MatButtonModule } from '@angular/material/button';
-import { PerspectiveCamera, Vector3 } from 'three';
 
 @Component({
     selector: 'app-rp-camera',
-    imports: [StlModelViewerComponent, MatButtonModule],
+    imports: [MatButtonModule],
     templateUrl: './rp-camera.component.html',
     styleUrl: './rp-camera.component.css'
 })
 export class RPCameraComponent {
 
-  cameras: PerspectiveCamera[] = [];
 
   constructor() {
-    for (let i = 0; i < 4; i++) {
-      this.cameras.push(new PerspectiveCamera(55, 1, 1, 15));
-      this.cameras[i].position.set(0, 3, 3);
-      this.cameras[i].lookAt(new Vector3(0,0,0));
-    }
-    
   }
   
 }
