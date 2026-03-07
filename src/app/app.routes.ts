df9396a390a66d76ef118c9694f33df16593b7b7
Gonzalo Oberreuter <gonzalo.oberreuter@gmail.com>
2026-03-07 19:26:46 +0100

    feat: add under construction component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { TrajectoryComponent } from './trajectory/trajectory.component';
-import { PlotsComponent } from './plots/plots.component';
-import { RPCameraComponent } from './rp-camera/rp-camera.component';
-import { OpenCVComponent } from './open-cv/open-cv.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
+// import { HomeComponent } from './home/home.component';
+// import { NotFoundComponent } from './not-found/not-found.component';
+// import { ProjectsComponent } from './projects/projects.component';
+// import { TrajectoryComponent } from './trajectory/trajectory.component';
+// import { PlotsComponent } from './plots/plots.component';
+// import { RPCameraComponent } from './rp-camera/rp-camera.component';
+// import { OpenCVComponent } from './open-cv/open-cv.component';
 
 export const routes: Routes = [
-    {path: "", title: "Home", component: HomeComponent},
-    {path: "projects", title: "Projects", component: ProjectsComponent},
-    {path: "projects/openCV", title: "OpenCV", component: OpenCVComponent},
-    {path: "projects/plots", title: "Plots", component: PlotsComponent},
-    {path: "projects/rp-camera", title: "RP Camera", component: RPCameraComponent},
-    {path: "trajectory", title: "Trajectory", component: TrajectoryComponent},
-    {path: "**", title: "404", component: NotFoundComponent}
+    // {path: "", title: "Home", component: HomeComponent},
+    // {path: "projects", title: "Projects", component: ProjectsComponent},
+    // {path: "projects/openCV", title: "OpenCV", component: OpenCVComponent},
+    // {path: "projects/plots", title: "Plots", component: PlotsComponent},
+    // {path: "projects/rp-camera", title: "RP Camera", component: RPCameraComponent},
+    // {path: "trajectory", title: "Trajectory", component: TrajectoryComponent},
+    {path: "**", title: "Under Construction", component: UnderConstructionComponent}
 
 ];
